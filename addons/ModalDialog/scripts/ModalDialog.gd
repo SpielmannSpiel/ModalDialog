d3b46163c7bcc5985765d183ad72a831e134675a
bison-- <mike.bison42@googlemail.com>
2025-07-31 09:43:24 +0200

    add separators for multi-options

--- addons/ModalDialog/scripts/ModalDialog.gd
+++ addons/ModalDialog/scripts/ModalDialog.gd
@@ -9,6 +9,13 @@ enum AutoWidthEn {
 	HALF_SCEEEN
 }
 
+enum SeparatorDirectionEn {
+	NONE = 0,
+	HORIZONTAL = 2,
+	VERTICAL = 4,
+	HORIZONTAL_VERTICAL = 6
+}
+
 signal dialog_shown()
 signal dialog_hidden()
 signal dialog_chosen(selected_option: ModalDialogOption)
@@ -16,7 +23,11 @@ signal dialog_chosen(selected_option: ModalDialogOption)
 ## Hides the dialog when the user picked an option.[br]
 ## Disable this to do it manually.
 @export var auto_hide: bool = true
+## Automatically calls the attached callable function[br]
+## (option_function_call) of an option.
+@export var auto_call_option_function: bool = true
 @export var auto_width: AutoWidthEn = AutoWidthEn.QUARTER_SCREEN
+@export_flags("Horizontal:2", "Vertical:4") var add_separators: int = 6
 @export var max_width: int = 0
 @export var dialog_title: Label
 @export var dialog_text: RichTextLabel
@@ -72,6 +83,13 @@ func hide_dialog() -> void:
 	
 func chosen(modal_option: ModalDialogOption) -> void:
 	dialog_chosen.emit(modal_option)
+	
+	if not modal_option.option_function_call.is_null():
+		modal_option.option_function_call.call(modal_option)
+	
+	if not modal_option.option_function_call_no_params.is_null():
+		modal_option.option_function_call_no_params.call()
+	
 	if auto_hide:
 		hide_dialog()
 
@@ -100,6 +118,28 @@ func add_new_button(option: ModalDialogOption, target: Node) -> void:
 	target.add_child(button)
 	button.owner = target
 
+func add_new_separator(direction: SeparatorDirectionEn, target: Node) -> void:
+	#if add_separators == SeparatorDirectionEn.NONE:
+	#	return
+	
+	#if not direction & add_separators:
+	#	return
+	
+	var separator: Control
+	match direction:
+		SeparatorDirectionEn.HORIZONTAL:
+			separator = HSeparator.new()
+		SeparatorDirectionEn.VERTICAL:
+			separator = VSeparator.new()
+	
+	target.add_child(separator)
+	separator.owner = target
+
+func add_new_dummy(target: Node) -> void:
+	var dummy: Control = Control.new()
+	target.add_child(dummy)
+	dummy.owner = target
+
 func _create_buttons_1() -> void:
 	add_new_button(_options[0], button_target)
 
@@ -109,6 +149,7 @@ func _create_buttons_2() -> void:
 	container.owner = button_target
 	
 	add_new_button(_options[0], container)
+	add_new_separator(SeparatorDirectionEn.VERTICAL, container)
 	add_new_button(_options[1], container)
 
 func _create_buttons_4() -> void:
@@ -117,10 +158,23 @@ func _create_buttons_4() -> void:
 	container.owner = button_target
 	
 	container.columns = 2
+	if SeparatorDirectionEn.VERTICAL & add_separators:
+		container.columns = 3
 	
 	add_new_button(_options[0], container)
+	if SeparatorDirectionEn.VERTICAL & add_separators:
+		add_new_separator(SeparatorDirectionEn.VERTICAL, container)
 	add_new_button(_options[1], container)
+	
+	if SeparatorDirectionEn.HORIZONTAL & add_separators:
+		add_new_separator(SeparatorDirectionEn.HORIZONTAL, container)
+		if SeparatorDirectionEn.VERTICAL & add_separators:
+			add_new_dummy(container)
+		add_new_separator(SeparatorDirectionEn.HORIZONTAL, container)
+	
 	add_new_button(_options[2], container)
+	if SeparatorDirectionEn.VERTICAL & add_separators:
+		add_new_separator(SeparatorDirectionEn.VERTICAL, container)
 	add_new_button(_options[3], container)
 
 func _create_buttons_list() -> void:
@@ -128,5 +182,13 @@ func _create_buttons_list() -> void:
 	button_target.add_child(container)
 	container.owner = button_target
 	
+	var loop_index: int = 0
+	var loop_index_max: int = _options.size() - 1
+	
 	for option in _options:
 		add_new_button(option, container)
+		if SeparatorDirectionEn.HORIZONTAL & add_separators:
+			if loop_index < loop_index_max:
+				add_new_separator(SeparatorDirectionEn.HORIZONTAL, container)
+		
+		loop_index += 1
